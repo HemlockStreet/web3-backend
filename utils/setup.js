482f546bd9e386ed00c514e2bdb0dab8ed48e401
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-11-24 12:41:44 -0600

    cors

--- utils/setup.js
+++ utils/setup.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const port = process.env.PORT ? process.env.PORT : 8080;
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const server = express();
+server.use(cors({ origin: '*' }));
 server.use(bodyParser.json());
 server.listen(port, () => console.log(`Listening On port:${port}\n`));
 
