907a004a96c4f824384ce45d8aabb4efe7d30294
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-12-12 17:56:59 -0600

    validation update (400)

--- routes/utils/validation.js
+++ routes/utils/validation.js
@@ -1,17 +1,6 @@
 function rejection(type, nature, res) {
-  const data = { info: `validation.${type} - ${nature}` };
-
-  let status;
-  if (
-    ['stolen', 'invalid', 'duplicate', '!authorized', 'expired'].includes(
-      nature
-    )
-  )
-    status = 403;
-  if (['missing', 'incomplete/ missing', 'invalid input'].includes(nature))
-    status = 401;
-
-  res.status(status).json(data);
+  const data = { info: `${type} - ${nature}` };
+  res.status(400).json(data);
 }
 
 module.exports = { rejection };
